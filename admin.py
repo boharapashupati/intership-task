4f7f90160fe731f27bff371fdbd53358dd5eadcf
Pashupati_bohara <bohara.pashupati123@gmail.com>
2025-07-05 10:45:22 +0545

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import ExpenseIncome
+
+@admin.register(ExpenseIncome)
+class ExpenseIncomeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'title', 'amount', 'transaction_type', 'tax_type', 'total', 'created_at']
+    list_filter = ['transaction_type', 'tax_type', 'created_at']
+    search_fields = ['title', 'description', 'user__username']
+    readonly_fields = ['total', 'created_at', 'updated_at']
+    
+    def total(self, obj):
+        return obj.total
+    total.short_description = 'Total Amount'
