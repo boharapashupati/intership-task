71786f7785d02e3b78811f9162cb39406cf303be
pasupati <bohara.pashupati123@gmail>
2025-07-06 07:54:47 +0545

    Added main project urls.py with Swagger routes

--- expenseapi/urls.py
+++ expenseapi/urls.py
@@ -18,15 +18,33 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from tracker.views import RegisterView
+from rest_framework import permissions
 from tracker.views import ExpenseIncomeViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+ ##SWAGGER
 
 router = DefaultRouter()
 router.register(r'expenses', ExpenseIncomeViewSet, basename='expenses')
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Expense Tracker API",
+      default_version='v1',
+      description="Track your income and expenses with tax logic",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="bohara.pashupati123@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
@@ -35,6 +53,10 @@ urlpatterns = [
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/register/', RegisterView.as_view(), name='register'),
+
+    # Swagger and Redoc URLs
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
